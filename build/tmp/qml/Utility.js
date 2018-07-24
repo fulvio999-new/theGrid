368c35640aff45c3783c45b3d17404890827e0f0
f4263 <fulvio.russo@guest.italiaonline.it>
2018-07-24 15:28:45 +0200

    refactoring

--- build/tmp/qml/Utility.js
+++
@@ -1,9 +0,0 @@
-
-    /* Depending on the Pagewidht of the Page (ie: the Device type) decide the Height of the scrollable */
-    function getContentHeight(){
-
-        if(root.width > units.gu(110))
-            return mainPage.height + mainPage.height/2 + units.gu(23)
-        else
-            return mainPage.height + mainPage.height/2 + units.gu(29) //phone
-    }
